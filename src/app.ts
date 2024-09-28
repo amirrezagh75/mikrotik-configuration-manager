74836801e1d461d6ac444ae10fa31c3eb6011aa3
backend3.safarbazi <bachend3.safarbazi@gmail.com>
2024-09-28 19:25:35 +0330

    :mute: Remove unused log

--- src/app.ts
+++ src/app.ts
@@ -18,27 +18,3 @@ app.listen(PORT, () => {
   console.log(`Server is running on: http://localhost:${PORT}`);
 });
 
-/*
-{
-  seq: '0',
-  host: '1.2.3.4',
-  status: 'timeout',
-  sent: '1',
-  received: '0',
-  'packet-loss': '100'
-}
-
-{
-  seq: '0',
-  host: '8.8.8.8',
-  size: '56',
-  ttl: '108',
-  time: '117ms232us',
-  sent: '1',
-  received: '1',
-  'packet-loss': '0',
-  'min-rtt': '117ms232us',
-  'avg-rtt': '117ms232us',
-  'max-rtt': '117ms232us'
-}
-*/
