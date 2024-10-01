f63ef18d0de35241ff644b33ff8c9c72746efee8
backend3.safarbazi <bachend3.safarbazi@gmail.com>
2024-10-01 18:33:49 +0330

    :label: Adding new type for tunnel

---
+++ src/modules/users/types/createVpn.interface.ts
@@ -0,0 +1,37 @@
+import z from 'zod'
+import { domainRegex, ipv4Regex } from '../../../utils';
+
+
+const vpnTypes = ["openVpn", "l2tp", "ppp", "pptp"] as const;
+const portRange = { min: 1, max: 65535 };
+
+const IpSchema = z.object({
+    address: z.string().refine(
+        (data) => ipv4Regex.test(data) || domainRegex.test(data), {
+        message: "Invalid IP address or domain format.",
+        path: ['address'],
+    }
+    ),
+    port: z.number().min(portRange.min, { message: `Port must be at least ${portRange.min}` })
+        .max(portRange.max, { message: `Port must be less than or equal to ${portRange.max}` })
+});
+
+const RouterSchema = z.object({
+    local: IpSchema.optional(),
+    public: IpSchema.optional(),
+    username: z.string(),
+    password: z.string(),
+});
+
+const CredentialSchema = z.object({
+    username: z.string(),
+    password: z.string()
+})
+
+export const CreateVpnSchema = z.object({
+    vpnType: z.enum(vpnTypes),
+    router: RouterSchema,
+    credential: CredentialSchema
+});
+
+export type CreateVpnInterface = z.infer<typeof CreateVpnSchema>;
--- src/modules/users/types/index.ts
+++ src/modules/users/types/index.ts
@@ -1,3 +1,4 @@
 export { RequestInputSchema, RequestInterface } from './request.interface'
 export { CreateTunnelSchema, CreateTunnelInterface } from './createTunnel.interface'
 export { PublicOrLocalIp } from './publicOrLocalIp.type'
+export { CreateVpnSchema as CreateOpenSchema, CreateVpnInterface as CreateOpenInterface } from './createVpn.interface'
