312b4d2ecf0c7abcddfa69a3fc9741c530d14dde
amirrezagh75 <mypersonal048@gmail.com>
2024-10-05 00:29:54 +0330

    :sparkles: new service for creating vpn on mikrotik

--- src/modules/mikrotik/services/index.ts
+++ src/modules/mikrotik/services/index.ts
@@ -1,3 +1,4 @@
 export { MikrotikService } from './mikrotik.service';
 export { MikrotikTunnelService } from './mikrotik.tunnel.service';
 export { MikrotikUtilService } from './mikrotik.utils.service'
+export { MikrotikVpnService } from './mikrotik.vpn.service'
